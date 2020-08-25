91df83f75960b3de5d84e21a69dc85cf91c99c2f
Tácio Tavares <taciogt@gmail.com>
2020-08-24 23:35:00 -0300

    faz o reverse como na resposta, com foldLeft

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -103,10 +103,7 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def length2[A](l: List[A]): Int = foldLeft(l, 0)((acc, _) => acc + 1)
 
-  def reverse[A](l: List[A]): List[A] = l match {
-    case Nil => Nil
-    case Cons(h, t) => append(reverse(t), Cons(h, Nil))
-  }
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, List[A]())((acc, h) => Cons(h, acc))
 
   def map[A, B](l: List[A])(f: A => B): List[B] = ???
 
